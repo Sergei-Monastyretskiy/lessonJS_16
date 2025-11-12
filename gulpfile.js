4ec8631876049c8db642b2cd861108aad4496d49
Sergei-Monastyretskiy <pringls777@gmail.com>
2025-11-12 23:42:39 +0200

    Added sourcemaps, enhanced autoprefixes for 5 browsers, modern CSS effects

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,27 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
 // Компіляція SCSS в CSS
 function styles() {
     return gulp.src('src/scss/style.scss')
-        .pipe(sass())
-        .pipe(autoprefixer())
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: [
+                'last 2 Chrome versions',
+                'last 2 Firefox versions',
+                'last 2 Safari versions',
+                'last 2 Edge versions',
+                'last 2 Opera versions',
+                '> 1%',
+                'not dead'
+            ],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.stream());
 }
@@ -21,10 +35,26 @@ function scripts() {
         .pipe(browserSync.stream());
 }
 
-// Мінімізація CSS
+// Мінімізація CSS для продакшну
 function minifyCSS() {
-    return gulp.src('css/style.css')
-        .pipe(cleanCSS())
+    return gulp.src('src/scss/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: [
+                'last 2 Chrome versions',
+                'last 2 Firefox versions',
+                'last 2 Safari versions',
+                'last 2 Edge versions',
+                'last 2 Opera versions',
+                '> 1%',
+                'not dead'
+            ],
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            level: 2,
+            specialComments: 0
+        }))
         .pipe(gulp.dest('css'));
 }
 
